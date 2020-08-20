7da3e05a14615bb50728dc70c14182f52e5f1841
Alex Root-Roatch <arootroatch@gmail.com>
2020-08-19 21:14:39 -0500

    delete AOS files. Delete onlick JS from servicesSection

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,8 +1,10 @@
 removeAnchors();
 window.addEventListener("resize", function() {
-    lax.updateElements();
     removeAnchors();
+    lax.updateElements();
 });
+
+
 
 
 //fader fun
@@ -125,123 +127,3 @@ function toggleNestedNav(){
     i.src = "https://cdn.curator.io/published/5c45e6ba-7cbc-48d2-be74-39dd75a45ccd.js";
     e = d.getElementsByTagName(s)[0];e.parentNode.insertBefore(i, e);
 })();
-
-//about section
-var elem1a = document.getElementById('caret1');
-var elem1b = document.getElementById('audio');
-var elem2a = document.getElementById('caret2');
-var elem2b = document.getElementById('lighting');
-var elem3a = document.getElementById('caret3');
-var elem3b = document.getElementById('repair');
-var elem4a = document.getElementById('caret4');
-var elem4b = document.getElementById('programming');
-var elemArray1 = [elem1a, elem2a, elem3a, elem4a];
-var elemArray2 = [elem1b, elem2b, elem3b, elem4b];
-
-
-function showAudio() {
-    var found = false;
-    if (elem1a.style.opacity == '0') {
-        for (i=0; i<4; i++) {
-            if (elemArray1[i].style.opacity == '1') {
-                elemArray1[i].style.opacity = '0';
-                elemArray2[i].style.width = '0%';
-                elemArray2[i].style.height = '25%';
-                found = true;
-                setTimeout(hello, 200);
-            } 
-        }
-
-        function hello() {
-            elem1a.style.opacity = '1';
-            elem1b.style.width = '100%';
-            elem1b.style.height = '100%';
-        }
-
-        if(found == false){
-            hello();
-        }
-    } 
-}
-
-function showLighting() {
-    var found = false;
-    if (elem2a.style.opacity == '0') {
-        for (i=0; i<4; i++) {
-            if (elemArray1[i].style.opacity == '1') {
-                elemArray1[i].style.opacity = '0';
-                elemArray2[i].style.width = '0%';
-                elemArray2[i].style.height = '25%';
-                found = true;
-                setTimeout(hello, 200);
-            } 
-        }
-        if (found == false){
-            hello();
-        }
-
-        function hello() {
-            elem2a.style.opacity = '1';
-            elem2b.style.width = '100%';
-            elem2b.style.height = '100%';
-        }
-    } 
-}
-
-function showRepairs() {
-    var found = false;
-    if (elem3a.style.opacity == '0') {
-        for (i=0; i<4; i++) {
-            if (elemArray1[i].style.opacity == '1') {
-                elemArray1[i].style.opacity = '0';
-                elemArray2[i].style.width = '0%';
-                elemArray2[i].style.height = '25%';
-                found = true;
-                setTimeout(hello, 200);
-            } 
-        }
-        if (found == false){
-            hello();
-        }
-        function hello(){
-            elem3a.style.opacity = '1';
-            elem3b.style.width = '100%';
-            elem3b.style.height = '100%';
-        }
-    } 
-}
-
-function showProgramming() {
-    var found = false;
-    if (elem4a.style.opacity == '0') {
-        for (i=0; i<4; i++) {
-            if (elemArray1[i].style.opacity == '1') {
-                elemArray1[i].style.opacity = '0';
-                elemArray2[i].style.width = '0%';
-                elemArray2[i].style.height = '25%';
-                found = true;
-                setTimeout(hello, 200);
-            } 
-        }
-        if (found == false){
-            hello();
-        }
-        function hello(){
-            elem4a.style.opacity = '1';
-            elem4b.style.width = '100%';
-            elem4b.style.height = '100%';
-        }
-    } 
-}
-
-//remove outline when opacity is 0, called in fader onscroll 
-// function removeOutline() {
-//     for (i=1; i<5; i++) {
-//         if (document.getElementById('fig' + i).style.opacity == '0') {
-//             document.getElementById('btn' + i).style.outline = 'none';
-//         } else {
-//             document.getElementById('btn' + i).style.outline = '';
-//         }
-//     }
-    
-// }
